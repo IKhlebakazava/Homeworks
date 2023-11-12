646b6b95b550ff9555c1f08f21cdc197de58ca47
Irina <khlebokazova@gmail.com>
2023-11-11 20:12:31 -0500

    Completed tasks for lesson 13

---
+++ lesson13/build/ts1.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const users = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep',
+        car: 'VW'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut',
+        children: 2
+    }
+];
